8cab56354a202ffaf2f79c10b205cd22d48c6264
Ruturaj Sant <sant.ruturaj2001@gmail.com>
2021-12-02 22:45:59 +0530

    added basic styles for sections & added footer

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const toggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelectorAll(".primary-navigation .nav-link");
+const body = document.querySelector("body");
 
 toggle.addEventListener("click", () => {
 	const visible = primaryNav.getAttribute("data-visible");
@@ -12,3 +14,20 @@ toggle.addEventListener("click", () => {
 		toggle.setAttribute("aria-expanded", false);
 	}
 });
+
+navLinks.forEach(navLink => {
+	navLink.addEventListener("click", () => {
+		navLinks.forEach(nav => {
+			nav.classList.remove("active");
+		});
+		navLink.classList.add("active");
+	});
+});
+
+handleScroll = () => {
+	if (body.classList.contains("on-scrollbar") === false) {
+		body.classList.add("on-scrollbar");
+	}
+};
+
+window.addEventListener("scroll", handleScroll, true);
