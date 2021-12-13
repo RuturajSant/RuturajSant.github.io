2c1a16e0882c07eb36173008b78b22b07e473c85
Ruturaj Sant <sant.ruturaj2001@gmail.com>
2021-12-13 22:15:07 +0530

    finished styles added loader

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const toggle = document.querySelector(".menu-toggle");
-const navbar = document.querySelector("nav");
 const navLinks = document.querySelectorAll(".primary-navigation .nav-link");
-const body = document.querySelector("body");
 const sections = document.querySelectorAll("section");
+const loader = document.querySelector(".loader");
 
+window.addEventListener("load", () => {
+	console.log("hi");
+	loader.classList.add("remove-loader");
+});
 // toggle menu
 toggle.addEventListener("click", () => {
 	const visible = primaryNav.getAttribute("data-visible");
-
 	if (visible === "false") {
 		primaryNav.setAttribute("data-visible", true);
 		toggle.setAttribute("aria-expanded", true);
@@ -34,12 +36,10 @@ const options = {
 
 let observer = new IntersectionObserver(navCheck, options);
 function navCheck(entries) {
-	console.log("hello");
 	entries.forEach(entry => {
 		const secName = entry.target.id;
 		const activeLink = document.querySelector(`[data-page=${secName}]`);
 		if (entry.isIntersecting) {
-			console.log(entry.target.id);
 			navLinks.forEach(nav => {
 				nav.classList.remove("active");
 			});
